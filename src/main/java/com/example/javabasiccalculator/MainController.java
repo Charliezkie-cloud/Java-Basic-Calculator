23e57a9cb4d947c6639519122c8f7e60f010f15f
Charliezkie-cloud <charleshenrytinoy8@gmail.com>
2025-09-04 07:51:01 +0800

    Update

--- src/main/java/com/example/javabasiccalculator/MainController.java
+++ src/main/java/com/example/javabasiccalculator/MainController.java
@@ -128,6 +128,24 @@ public class MainController {
     }
 
     @FXML
+    private void clearHistoryOnClicked() {
+        historyBox.getChildren().removeAll();
+    }
+
+    @FXML
+    private void clearOnClicked() {
+        operandsInput.appendText("");
+    }
+
+    @FXML
+    private void allClearOnClicked() {
+        firstOperand = 0;
+        operator = '0';
+        secondOperand = 0;
+        operandsInput.appendText("");
+    }
+
+    @FXML
     private void keyPeriodOnClicked() {
         operandsInput.appendText(".");
     }
